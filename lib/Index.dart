fffa0e896b3e8b98b3f0462a1d702210036d9f84
Wilder Lizama Garcia <wilderlizama@gmail.com>
2020-03-24 23:54:02 -0500

    init pantallas

---
+++ lib/Index.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Index extends StatelessWidget {
+	@override
+	Widget build(BuildContext context) {
+		return Scaffold(
+			appBar: AppBar(
+				title: Text('Index'),
+			),
+			body: Center(
+				child: Text('texto de prueba'),
+			),
+		);
+	}
+}
