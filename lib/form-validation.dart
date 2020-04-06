28d8682c7b7f1a7a460cd3efed41c49b26ce0919
Wilder Lizama Garcia <wilderlizama@gmail.com>
2020-04-05 22:39:51 -0500

    Focus inputs

--- lib/form-validation.dart
+++ lib/form-validation.dart
@@ -20,6 +20,21 @@ class FirstFormValidation extends StatefulWidget {
 
 class _FirstFormValidationInputState extends State<FirstFormValidation> {
   final _formkey = GlobalKey<FormState>();
+  FocusNode myFocusNode;
+
+  @override
+  void initState() {
+    super.initState();
+    // crea el focus node
+    myFocusNode = FocusNode();
+  }
+
+  @override
+  void dispose() {
+    // limpia focus node cuando se cierre el formulario
+    myFocusNode.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,38 +46,63 @@ class _FirstFormValidationInputState extends State<FirstFormValidation> {
         builder: (BuildContext context) {
           return Form(
             key: _formkey,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                TextFormField(
-                  decoration: InputDecoration(
-                    // border: InputBorder.none,
-                    hintText: 'Ejm: Wily Colon',
-                    labelText: 'Nombre a buscar'
-                  ),
-                  validator: (value) {
-                    if (value.isEmpty) {
-                      return 'Este valor no puede estar vacio';
-                    }
-                  },
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 16.0),
-                  child: RaisedButton(
-                    onPressed: () {
-                      if (_formkey.currentState.validate()) {
-                        Scaffold.of(context).showSnackBar(
-                          SnackBar(
-                            content: Text('Procesando'),
-                          )
-                        );
+            child:  Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  TextFormField(
+                    autofocus: true,
+                    focusNode: myFocusNode,
+                    decoration: InputDecoration(
+                      hintText: 'Ejm: Wily Colon',
+                      labelText: 'Nombre'
+                    ),
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Este valor no puede estar vacio';
                       }
                     },
-                    child: Text('submit'),
                   ),
-                )
-              ],
-            ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: '00',
+                      labelText: 'Edad'
+                    ),
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: 'Ejm: wily.colon@gmail.com',
+                      labelText: 'e-mail'
+                    ),
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Este valor no puede estar vacio';
+                      }
+                    },
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 16.0),
+                    child: RaisedButton(
+                      onPressed: () {
+                        if (_formkey.currentState.validate()) {
+                          Scaffold.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text('Procesando'),
+                            )
+                          );
+                        }
+                        else {
+                          FocusScope.of(context).requestFocus(myFocusNode);
+                        }
+                      },
+                      child: Text('submit'),
+                    ),
+                  )
+                ],
+              ),
+            ) 
+            
           );
         }
       )
